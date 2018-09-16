5afa6b79b8375758f435ff465786951379090132
Michael Frieze <michaelfrieze@icloud.com>
2018-09-15 20:17:47 -0400

    Added and updated some components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
 import './App.css';
 
@@ -7,8 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333" />
       </div>
     );
   }
