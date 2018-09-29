a3eb45b0f17b8fd85e7cc727a47523d2369a18d2
Michael Frieze <michaelfrieze@icloud.com>
2018-09-29 16:27:49 -0400

    added ability to delete contacts by using context for state management

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
 
-// so here we have to import our provider
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +10,6 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      // now we have to wrap this single "App" div in the Provider
       <Provider>
         <div className="App">
           <Header branding="Contact Manager" />
