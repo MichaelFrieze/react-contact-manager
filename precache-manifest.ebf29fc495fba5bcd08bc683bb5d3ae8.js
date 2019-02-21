00e94061ef4e2e2bb1ed58b20669e8bb54817735
Michael Frieze <michaelfrieze@icloud.com>
2019-02-21 00:21:06 -0500

    Updates

---
+++ precache-manifest.ebf29fc495fba5bcd08bc683bb5d3ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "156cf5b10d56406c8ced",
+    "url": "/react-contact-manager/static/js/runtime~main.156cf5b1.js"
+  },
+  {
+    "revision": "f911a10a815acb8315cc",
+    "url": "/react-contact-manager/static/js/main.f911a10a.chunk.js"
+  },
+  {
+    "revision": "1038f5d980e7eb1efd6e",
+    "url": "/react-contact-manager/static/js/2.1038f5d9.chunk.js"
+  },
+  {
+    "revision": "f911a10a815acb8315cc",
+    "url": "/react-contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1038f5d980e7eb1efd6e",
+    "url": "/react-contact-manager/static/css/2.802a652a.chunk.css"
+  },
+  {
+    "revision": "96aa95ce7ea936ee0c631427607ff6c4",
+    "url": "/react-contact-manager/index.html"
+  }
+];
