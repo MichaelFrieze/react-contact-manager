e725161117fb727014c869d783476a00e5556342
Michael Frieze <michaelfrieze@icloud.com>
2019-02-21 21:49:06 -0500

    cleaned up comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,6 @@
-// This is a meeting place for all of our reducers
-// there will often times be more reducers in this directory
 import { combineReducers } from "redux";
 import contactReducer from "./contactReducer";
 
 export default combineReducers({
   contact: contactReducer
-  // this name contact is important
-  // when we bring in our state from redux into a component, this will be available in the this.props
-  // So, this.props.contact
 });
