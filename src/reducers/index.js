1e854889b308ff3a44fb26b967590d35dde0e2a2
Michael Frieze <michaelfrieze@icloud.com>
2019-02-21 21:48:41 -0500

    created main index file for the reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
+// This is a meeting place for all of our reducers
+// there will often times be more reducers in this directory
 import { combineReducers } from "redux";
 import contactReducer from "./contactReducer";
 
 export default combineReducers({
-  // the name contact is important
   contact: contactReducer
+  // this name contact is important
+  // when we bring in our state from redux into a component, this will be available in the this.props
+  // So, this.props.contact
 });
